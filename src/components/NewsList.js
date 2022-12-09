f301df87fbec025e8f8aaad61949163a9b19d046
Aishwarya <saichaudharid@gmail.com>
2022-12-09 12:16:59 +0530

    first commit

---
+++ src/components/NewsList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import NewsItem from './NewsItem'
+// require('dotenv').config({ path: '../../.env' })
+
+const NewsList = () => {
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        const getArticles = async () => {
+            const response = await axios.get(`https://newsapi.org/v2/everything?q=football&apiKey=efc1372428514cabb09908d9a0d9c518`)
+            setArticles(response.data.articles)
+            console.log(response)
+        }
+
+        getArticles()
+    }, [])
+    return (
+        <div>
+            {articles.map(article => {
+                return(
+                    <NewsItem 
+                        title={article.title}
+                        description={article.description}
+                        url={article.url}
+                        urlToImage={article.urlToImage} 
+                    />
+                )
+            })}
+        </div>
+    )
+}
+
+export default NewsList
